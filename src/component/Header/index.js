66fc7cef0766dc8e1674e8a86f6c8aa1a673a452
ricky5556 <skumar172@sapient.com>
2017-02-02 19:15:04 +0530

    First Run

--- src/component/Header/index.js
+++ src/component/Header/index.js
@@ -1,3 +1,14 @@
-/*
-* Header component
-*/
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <span className='bib-logo'>
+      </span>
+      <h1>Bibliophile</h1>
+    </div>
+
+  )
+}
+
+export default Header
--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-import React, { Component } from 'react'
+/* import libs */
+import React from 'react'
 import { render } from 'react-dom'
+import Header from './component/Header'
+/* import styles */
+import './index.scss'
 
-render(<div> Bibliophile </div>)
+const renderMain = render(
+    <div>
+      <Header/>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>,
+    document.getElementById("bib-root")
+  )
 
+export default renderMain
